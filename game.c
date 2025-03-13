0b8c3968304a2ae2a6e2e2cebeb66e0cea172b2e
ToxikSkrrt <thomalex47240@gmail.com>
2025-03-13 11:07:50 +0100

    added sleeps

--- game.c
+++ game.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "monster.h"
 #include "player.h"
@@ -14,16 +15,34 @@ void monster_attack(monster_t monster, player_t player) {
 }
 
 void runGame() {
+  system("clear");
   // bool isRunning = true;
+  sleep(1);
+  printf("START GAME");
+  fflush(stdout);
+
+  for (int i = 0; i < 3; i++) {
+    sleep(1);
+    printf(".");
+    fflush(stdout);
+  }
+
+  sleep(3);
+
+  printf("\n");
 
   player_t player = player_create("Toxik", 1, 10, 2, 0);
   printf("NEW PLAYER CREATED !\n");
+  sleep(1);
   player_showInfo(player);
+
+  sleep(2);
 
   printf("\n\n");
 
   monster_t monster = monster_create("Garrosh", 1, 5, 1, 1);
   printf("NEW MONSTER CREATED !\n");
+  sleep(1);
   monster_showInfo(monster);
 
   // while (isRunning)
