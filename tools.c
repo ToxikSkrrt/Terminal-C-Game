2871db15d8b5cd7840ad471f7496f769d5735c31
Thomas-Alexandre Moreau <thomalex.moreau@gmail.com>
2025-03-13 18:07:51 +0100

    tools files

---
+++ tools.c
@@ -0,0 +1,38 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <termios.h>
+#include <time.h>
+#include <unistd.h>
+
+void consoleClear() {
+  system("clear");
+}
+
+char getch() {
+  struct termios oldt, newt;
+  char ch;
+  tcgetattr(STDIN_FILENO, &oldt);
+  newt = oldt;
+  newt.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  ch = getchar();
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+  return ch;
+}
+
+void sleep_ms(int milliseconds) {
+  struct timespec ts;
+  ts.tv_sec = milliseconds / 1000;               // Convert to seconds
+  ts.tv_nsec = (milliseconds % 1000) * 1000000L; // Convert to nanoseconds
+  nanosleep(&ts, NULL);
+}
+
+void sleepPrint(char *input, int delay) {
+  for (int i = 0; i < strlen(input); i++) {
+    printf("%c", input[i]);
+    fflush(stdout);
+    sleep_ms(delay);
+  }
+}
---
+++ tools.h
@@ -0,0 +1,9 @@
+#ifndef __TOOLS_H__
+#define __TOOLS_H__
+
+void consoleClear();
+char getch();
+void sleep_ms(int milliseconds);
+void sleepPrint(char *input, int delay);
+
+#endif
