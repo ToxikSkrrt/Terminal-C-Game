a6d61e9e1f68d7f6ae5c7a7eaaf96765fe33b317
ToxikSkrrt <thomalex47240@gmail.com>
2025-03-19 12:56:22 +0100

    modify to just enable all linux terminal

--- main.c
+++ main.c
@@ -7,7 +7,6 @@
 #include "player.h"
 
 const char *detect_terminal() {
-#ifdef __linux__
   if (system("which gnome-terminal > /dev/null 2>&1") == 0) {
     return "gnome-terminal --window --full-screen --zoom=1.7 -- bash -c './main new_terminal'";
   } else if (system("which konsole > /dev/null 2>&1") == 0) {
@@ -15,13 +14,7 @@ const char *detect_terminal() {
   } else {
     return "xterm -maximized -fa 'Monospace' -fs 14 -e './main new_terminal'";
   }
-#elif __APPLE__
-  return "osascript -e 'tell application \"Terminal\" to do script \"./main new_terminal\"' -e 'tell application \"Terminal\" to activate'";
-#elif _WIN32
-  return "start /max cmd /c main.exe";
-#else
   return NULL; // Unsupported OS
-#endif
 }
 
 int main(int argc, char **argv) {
